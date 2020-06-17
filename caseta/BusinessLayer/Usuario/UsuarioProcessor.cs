fa55018c84cf1d263d1fa57e324b80311f9f1ffc
Esteban Aburto <juan.aburto@axity.com>
2020-06-17 18:05:54 -0500

    Usuario Inicio

---
+++ caseta/BusinessLayer/Usuario/UsuarioProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessInterfaces;
+using DataInterfaces;
+
+namespace BusinessLayer
+{
+    public class UsuarioProcessor : IUsuarioProcessor
+    {
+        private readonly IUsuarioRepositorio UsuarioRepositorio;
+        public UsuarioProcessor(IUsuarioRepositorio _usuarioRepositorio)
+        {
+            UsuarioRepositorio = _usuarioRepositorio;
+        }
+        public bool UserExist(string usuario, string contraseña)
+        {
+            var user = UsuarioRepositorio.GetUsuario(usuario, contraseña);
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
